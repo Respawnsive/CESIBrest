b7cca06078a29f05ac3c656d743bc823a71c7617
Jérôme Liger <jerome.liger@respawnsive.com>
2022-02-21 17:03:38 +0100

    Correction compilation Ok

--- KaamelotSampler/KaamelotSampler/KaamelotApp.xaml.cs
+++ KaamelotSampler/KaamelotSampler/KaamelotApp.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using KaamelotSampler.Views;
+using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -10,7 +11,7 @@ namespace KaamelotSampler
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new ListeSaamplesPage();
         }
 
         protected override void OnStart()
