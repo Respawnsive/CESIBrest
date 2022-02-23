d61a26428c539968cfee6b033f40534398ea071a
Jérôme Liger <jerome.liger@respawnsive.com>
2022-02-23 09:31:32 +0100

    Ajout du renderer Entry

---
+++ KaamelotSampler/KaamelotSampler.Android/Renderers/MyCustomEntryAndroidRenderer.cs
@@ -0,0 +1,34 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using KaamelotSampler.CustomCtrl;
+using KaamelotSampler.Droid.Renderers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(MyCustomEntry), typeof(MyCustomEntryAndroidRenderer))]
+namespace KaamelotSampler.Droid.Renderers
+{
+    public class MyCustomEntryAndroidRenderer : EntryRenderer
+    {
+        public MyCustomEntryAndroidRenderer(Context context) : base(context)
+        {
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+            if (Control != null)
+            {
+                Control.SetBackgroundColor(Android.Graphics.Color.Transparent);
+            }
+        }
+    }
+}
---
+++ KaamelotSampler/KaamelotSampler.iOS/Renderers/MyCustomEntryiOSRenderer.cs
@@ -0,0 +1,26 @@
+﻿using Foundation;
+using KaamelotSampler.CustomCtrl;
+using KaamelotSampler.iOS.Renderers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly:ExportRenderer(typeof(MyCustomEntry), typeof(MyCustomEntryiOSRenderer))]
+namespace KaamelotSampler.iOS.Renderers
+{
+    public class MyCustomEntryiOSRenderer : EntryRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+            if (Control != null)
+            {
+                Control.BorderStyle = UITextBorderStyle.None;
+            }
+        }
+    }
+}
